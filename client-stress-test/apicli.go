5b33fdfe69bb4e74852a791e67026be049fedcbd
Gustavo Trott <gustavo@trott.com.br>
2023-04-26 17:26:44 -0300

    Add program to stress the client with users and chat msgs

---
+++ client-stress-test/apicli.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+type CreateResponse struct {
+	XMLName           xml.Name `xml:"response"`
+	Returncode        string   `xml:"returncode"`
+	Message           string   `xml:"message"`
+	MeetingID         string   `xml:"meetingID"`
+	InternalMeetingID string   `xml:"internalMeetingID"`
+}
+
+func requestApiCreate(client *http.Client) string {
+	voiceBridge := getRandomIntegerAsString()
+	extMeetingId := "test-" + getRandomIntegerAsString()
+	controller := "create"
+	params := "attendeePW=ap&meetingID=" + extMeetingId + "&moderatorPW=mp&name=" + extMeetingId + "&voiceBridge=" + voiceBridge + "&welcome=Heeyyy"
+
+	createUrl := getApiUrl() + "/" + controller + "?" + params + "&checksum=" + getSha1sum(controller+params+getSalt())
+
+	respCreate, err := client.Get(createUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer respCreate.Body.Close()
+	bodyCreate, err := ioutil.ReadAll(respCreate.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var objCreateResponse CreateResponse
+	errCreateXmlParse := xml.Unmarshal(bodyCreate, &objCreateResponse)
+	if errCreateXmlParse != nil {
+		fmt.Printf("error: %v", errCreateXmlParse)
+		return ""
+	}
+
+	if objCreateResponse.Returncode != "SUCCESS" {
+		fmt.Println("Error on trying to create meeting: " + objCreateResponse.Message)
+		return ""
+	}
+	fmt.Println("Meeting " + objCreateResponse.MeetingID + " created successfully.")
+
+	return objCreateResponse.InternalMeetingID
+}
+
+type JoinResponse struct {
+	XMLName      xml.Name `xml:"response"`
+	Returncode   string   `xml:"returncode"`
+	Message      string   `xml:"message"`
+	UserId       string   `xml:"user_id"`
+	AuthToken    string   `xml:"auth_token"`
+	SessionToken string   `xml:"session_token"`
+	Url          string   `xml:"url"`
+}
+
+func generateJoinUrl(meetingId string, name string, redirect string) string {
+	controller := "join"
+	name = strings.Replace(name, " ", "+", -1)
+	params := "fullName=" + name + "&meetingID=" + meetingId + "&password=mp&redirect=" + redirect
+	return getApiUrl() + "/" + controller + "?" + params + "&checksum=" + getSha1sum(controller+params+getSalt())
+}
+
+func requestApiJoin(client *http.Client, meetingId string, name string) (string, string, string) {
+	respJoin, err := client.Get(generateJoinUrl(meetingId, name, "false"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer respJoin.Body.Close()
+	bodyJoin, err := ioutil.ReadAll(respJoin.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var objJoinResponse JoinResponse
+	errJoinXmlParse := xml.Unmarshal(bodyJoin, &objJoinResponse)
+	if errJoinXmlParse != nil {
+		fmt.Printf("error: %v", errJoinXmlParse)
+		return "", "", ""
+	}
+
+	if objJoinResponse.Returncode != "SUCCESS" {
+		fmt.Println("Error on trying to create user: " + objJoinResponse.Message)
+		return "", "", ""
+	}
+	fmt.Println("User " + objJoinResponse.UserId + " created successfully.")
+
+	return objJoinResponse.UserId, objJoinResponse.SessionToken, objJoinResponse.AuthToken
+}
+
+type ResponseApiEnterData struct {
+	ReturnCode string `json:"returncode"`
+	Message    string `json:"message"`
+	MessageKey string `json:"messageKey"`
+	LogoutURL  string `json:"logoutURL"`
+	Fullname   string `json:"fullname"`
+}
+
+type ApiEnterResponse struct {
+	Response ResponseApiEnterData `json:"response"`
+}
+
+func requestApiEnter(client *http.Client, sessionToken string) {
+	controller := "enter"
+	enterUrl := getApiUrl() + "/" + controller + "?sessionToken=" + sessionToken
+
+	resp, err := client.Get(enterUrl)
+	if err != nil {
+		log.Fatalf("Erro ao fazer a requisição: %v", err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Erro ao ler o corpo da resposta: %v", err)
+	}
+
+	var apiResponse ApiEnterResponse
+	if err := json.Unmarshal(body, &apiResponse); err != nil {
+		log.Fatalf("Erro ao decodificar o JSON: %v", err)
+	}
+
+	fmt.Printf("ReturnCode: %s\n", apiResponse.Response.ReturnCode)
+
+	if apiResponse.Response.ReturnCode != "SUCCESS" {
+		fmt.Printf("Message: %s\n", apiResponse.Response.Message)
+		return
+	}
+
+	fmt.Printf("Fullname: %s\n", apiResponse.Response.Fullname)
+}
---
+++ client-stress-test/main.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"crypto/sha1"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"net/http/cookiejar"
+	"os"
+	"strconv"
+	"time"
+)
+
+type Config struct {
+	AppName                        string   `json:"app_name"`
+	SecuritySalt                   string   `json:"securitySalt"`
+	NumOfUsers                     int      `json:"numOfusers"`
+	IntervalBetweenMessagesInMs    int      `json:"intervalBetweenMessagesInMs"`
+	MinIntervalBetweenUserJoinInMs int      `json:"minIntervalBetweenUserJoinInMs"`
+	MaxIntervalBetweenUserJoinInMs int      `json:"maxIntervalBetweenUserJoinInMs"`
+	ListOfMessages                 []string `json:"listOfMessages"`
+	Debug                          bool     `json:"debug"`
+}
+
+func getConfig() Config {
+	file, err := os.Open("config.json")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var config Config
+	err = json.NewDecoder(file).Decode(&config)
+	if err != nil {
+		panic(err)
+	}
+
+	return config
+}
+
+func getApiUrl() string {
+	return "https://bbb27.bbbvm.imdt.com.br/bigbluebutton/api"
+}
+
+func getSalt() string {
+	//	if len(os.Args) < 2 {
+	//		fmt.Println("Use: ./bbb-stress-test [secret salt]")
+	//		os.Exit(1)
+	//	}
+	//	return os.Args[1]
+
+	config := getConfig()
+	return config.SecuritySalt
+}
+
+func getSha1sum(input string) string {
+	hasher := sha1.New()
+	hasher.Write([]byte(input))
+	sha1sum := hasher.Sum(nil)
+	return hex.EncodeToString(sha1sum)
+}
+
+func getRandomIntegerAsString() string {
+	rand.Seed(time.Now().UnixNano())
+	return strconv.Itoa(rand.Intn(900000) + 100000)
+}
+
+func getTimestamp() int64 {
+	now := time.Now()
+	return now.UnixNano() / int64(time.Millisecond)
+}
+
+var currNumOfMsgs int64 = 1
+
+func addNewUser(client *http.Client, meetingId string, name string) {
+
+	userId, sessionToken, authToken := requestApiJoin(client, meetingId, name)
+	println("sessionToken:" + sessionToken)
+	//enterUser(client, sessionToken)
+
+	if userId == "" {
+		println("It was not possible to add the user " + name)
+		return
+	}
+
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("Sending Redis msg")
+
+	sendValidateAuthTokenReqMsg(meetingId, userId, authToken)
+	time.Sleep(1 * time.Second)
+	sendUserJoinMeetingReqMsg(meetingId, userId, authToken)
+	time.Sleep(1 * time.Second)
+
+	var config = getConfig()
+
+	chatMessages := config.ListOfMessages
+	if len(chatMessages) == 0 {
+		return
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	numOfMessages := rand.Intn(len(chatMessages)) + 1
+	for i := 0; i < numOfMessages; i++ {
+		sendSendGroupChatMessageMsg(meetingId, userId, strconv.FormatInt(currNumOfMsgs, 10)+" "+chatMessages[i])
+		currNumOfMsgs++
+		time.Sleep(time.Duration(config.IntervalBetweenMessagesInMs) * time.Millisecond)
+	}
+	time.Sleep(5 * time.Second)
+	for i := 0; i < numOfMessages; i++ {
+		sendSendGroupChatMessageMsg(meetingId, userId, strconv.FormatInt(currNumOfMsgs, 10)+" "+chatMessages[i])
+		currNumOfMsgs++
+		time.Sleep(time.Duration(config.IntervalBetweenMessagesInMs) * time.Millisecond)
+	}
+	time.Sleep(5 * time.Second)
+	for i := 0; i < numOfMessages; i++ {
+		sendSendGroupChatMessageMsg(meetingId, userId, strconv.FormatInt(currNumOfMsgs, 10)+" "+chatMessages[i])
+		currNumOfMsgs++
+		time.Sleep(time.Duration(config.IntervalBetweenMessagesInMs) * time.Millisecond)
+	}
+}
+
+func main() {
+	jar, _ := cookiejar.New(nil)
+	client := &http.Client{
+		Jar: jar,
+	}
+
+	config := getConfig()
+
+	meetingId := requestApiCreate(client)
+
+	println("Meeting id: ", meetingId)
+
+	println("Use this link to join the meeting in your browser: " + generateJoinUrl(meetingId, "Teacher", "true"))
+
+	fmt.Println(fmt.Sprintf("It will add %d users to the meeting.", config.NumOfUsers))
+
+	time.Sleep(5 * time.Second)
+
+	for i := 0; i < config.NumOfUsers; i++ {
+		name := fmt.Sprintf("Student %0*d", 5, i)
+		go addNewUser(client, meetingId, name)
+
+		rand.Seed(time.Now().UnixNano())
+		delayBetweenJoins := rand.Intn(config.MaxIntervalBetweenUserJoinInMs-config.MinIntervalBetweenUserJoinInMs+1) + config.MaxIntervalBetweenUserJoinInMs
+		time.Sleep(time.Duration(delayBetweenJoins) * time.Millisecond)
+	}
+
+	println("Waiting to finish....")
+
+	time.Sleep(1 * time.Minute)
+
+}
